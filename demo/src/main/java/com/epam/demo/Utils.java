687cb092baa1754359f0475223dbb8396da85a66
VladimirKameko <drugs1360@gmail.com>
2023-05-25 20:27:03 +0300

    first commit

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,13 @@
 package com.epam.demo;
 
 import java.util.List;
-
+import com.epam.utils.StringUtils;
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        //magic happens here
+        for (String st:args) {
+            if(!StringUtils.isPositiveNumber(st))
+                return false;
+        }
+        return true;
     }
 }
