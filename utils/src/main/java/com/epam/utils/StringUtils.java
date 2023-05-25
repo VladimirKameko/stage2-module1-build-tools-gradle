4a4272d0cc4396ff7a69320cd7068d5649db55e5
VladimirKameko <drugs1360@gmail.com>
2023-05-25 21:04:18 +0300

    last commit

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,8 +2,14 @@ package com.epam.utils;
 
 import org.apache.commons.lang3.math.NumberUtils;
 
+import java.util.List;
+
 public class StringUtils {
+
     public static boolean isPositiveNumber(String str) {
+        if(str.length() > 1 && str.charAt(0) == '0' && str.charAt(1) != '.'){
+            return false;
+        }
         return NumberUtils.toDouble(str) > 0;
     }
 }
